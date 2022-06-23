19bcd7310601a2496e5db3a616fd4238ce034db5
Ivan Lopez <ivaan100@gmail.com>
2022-06-23 13:10:25 +0200

    Formateo de numeros a dinero

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,18 +1,34 @@
 const ControlPresupuesto = ({ presupuesto }) => {
-  return (
-    <div className="contenedor-presupuesto contenedor sombra dos-columnas">
-        <div>
-            <p>Gráfica aquí</p>
-        </div>
 
-        <div className="contenido-presupuesto">
-            <p>
-                <span>Presupuesto: </span> ${presupuesto}
-            </p>
-        </div>
+    const formatearCantidad = (cantidad) => {
+        return cantidad.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        })
+    }
 
-    </div>
-  )
+    return (
+        <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+            <div>
+                <p>Gráfica aquí</p>
+            </div>
+
+            <div className="contenido-presupuesto">
+                <p>
+                    <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
+                </p>
+
+                <p>
+                    <span>Disponible: </span> {formatearCantidad(0)}
+                </p>
+
+                <p>
+                    <span>Gastado: </span> {formatearCantidad(0)}
+                </p>
+            </div>
+
+        </div>
+    )
 }
 
 export default ControlPresupuesto
